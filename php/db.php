1ff7d466fb7b7a15ee48cd0cd7cbb8a7a6d6cd84
Bohdan <redliz.redliz@gmail.com>
2024-01-18 17:23:49 +0200

    New images have been added for the 30x30 page.

--- php/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../../../config/config.php");
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
